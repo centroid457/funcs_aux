2884ceda488d25bff159bc1fa4b06008509cd1e4
Andrei Starichenko <centroid@mail.ru>
2024-05-17 16:22:49 +0300

    zero rename internal module

--- funcs_aux/__init__.py
+++ funcs_aux/__init__.py
@@ -2,4 +2,5 @@ from .arrays import array_2d_get_compact_str
 from .iterables import Iterables
 from .results import ResultValue, ResultFunc, ResultExpect_Step, ResultExpect_Chain, ResultExpect_Base
 from .strings import Strings
-from .collects import NamesIndexed_Base, NamesIndexed_Templated, NamesIndexed_Example
+from .breeder_names import NamesIndexed_Base, NamesIndexed_Templated, NamesIndexed_Example
+from .breeder_objects import BreederObjectList, BreederObjectList_GroupType

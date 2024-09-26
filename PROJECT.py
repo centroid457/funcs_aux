1f83c1f7edfe7b9204a99b5fe583d119cca42c4e
Andrei Starichenko <centroid@mail.ru>
2024-09-26 11:18:59 +0300

    ready v0.4.26

--- PROJECT.py
+++ PROJECT.py
@@ -86,7 +86,7 @@ Designed to collect useful funcs in one place!
     ]
 
     # HISTORY -----------------------------------------------
-    VERSION: tuple[int, int, int] = (0, 4, 25)
+    VERSION: tuple[int, int, int] = (0, 4, 26)
     TODO: list[str] = [
         "..."
     ]
@@ -94,8 +94,7 @@ Designed to collect useful funcs in one place!
         "..."
     ]
     NEWS: list[str] = [
-        "[ValidChains] fix FailStop incorrect breakOut",
-        "rename to ValueNotExist"
+        "[ValueUnit] add ValueNotExist for using object as pattern"
     ]
 
 
